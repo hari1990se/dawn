e70e815fd0e3b0eadef66f4940c5e2ba6b229ecf
unknown <hshanker@IN-HShanker-LT.infinera.com>
2016-07-10 13:26:20 +0530

    new files

--- HackerRank/30DaysOfCoding/LetsReview.java
+++ HackerRank/30DaysOfCoding/LetsReview.java
@@ -8,11 +8,19 @@ class LetsReview
 		int T = scanner.nextInt();
 		while(T-- > 0)
 		{
-			String str = scanner.nextLine();
-			String odd = null;
-			String even = null;
-			for(char ch : str.toCharArray())
-				
+			Scanner scan = new Scanner(System.in);
+			String str = scan.nextLine();
+			StringBuilder odd = new StringBuilder();
+			StringBuilder even = new StringBuilder();
+			for(int i = 0; i < str.length(); i++)
+			{
+				even.append(str.charAt(i));
+				if(i < str.length())
+				{
+					odd.append(str.charAt(++i));
+				}
+			}
+			System.out.println(even.toString()+ " " +odd.toString());
 		}
 	}
 }
