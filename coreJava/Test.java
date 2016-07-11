8abfd8e5d00bfcb71b42ec430a90a9c032489b54
unknown <hshanker@IN-HShanker-LT.infinera.com>
2016-07-11 17:56:56 +0530

    Test.java

---
+++ coreJava/Test.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Test
+{
+    public static void main(String... strings)
+    {
+        int x, y;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextInt();
+        y = scanner.nextInt();
+        System.out.println(x + y);
+    }
+}
